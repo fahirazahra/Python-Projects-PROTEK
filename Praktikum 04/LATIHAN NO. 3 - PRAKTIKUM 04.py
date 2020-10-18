6d60d7c041d30fe8319e7f8c3c19ba55299bb5df
Fahira Zahra <fahirazahra@student.uns.ac.id>
2020-10-18 14:06:50 +0700

    Create LATIHAN NO. 3 - PRAKTIKUM 04.py

---
+++ Praktikum 04/LATIHAN NO. 3 - PRAKTIKUM 04.py
@@ -0,0 +1,7 @@
+# script hitung jumlah mengisi bensin pada mobil dari kota A ke kota C
+# dengan kapasitas tangki mobil adalah 50 liter
+
+bensin = 66.25
+kapasitasTangki = 50
+jumlahMengisi = bensin/kapasitasTangki
+print(jumlahMengisi)
