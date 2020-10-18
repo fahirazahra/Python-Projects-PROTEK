406ed0318582d01299c66e3887a5bce49f15a6d3
Fahira Zahra <fahirazahra@student.uns.ac.id>
2020-10-18 14:06:47 +0700

    Create LATIHAN NO. 2 - PRAKTIKUM 04.py

---
+++ Praktikum 04/LATIHAN NO. 2 - PRAKTIKUM 04.py
@@ -0,0 +1,7 @@
+# script hitung jumlah bensin mobil (dalam satuan liter)
+# dengan jarak kota A ke kota C adalah 795 km dan perbandingan bensin 1(liter):12(km)
+
+jarakKota = 795
+jarakBensin = 12
+literBensin = jarakKota/jarakBensin
+print(literBensin)
