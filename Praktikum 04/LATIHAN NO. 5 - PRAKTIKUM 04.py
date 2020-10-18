9c870e61d9355b7fff417c1b774c820e1a9e39cd
Fahira Zahra <fahirazahra@student.uns.ac.id>
2020-10-18 14:06:54 +0700

    Create LATIHAN NO. 5 - PRAKTIKUM 04.py

---
+++ Praktikum 04/LATIHAN NO. 5 - PRAKTIKUM 04.py
@@ -0,0 +1,8 @@
+# Jumlah Mahasiswa PTIK UNS 2020
+
+JumlahMahasiswaLakiLaki = 57
+JumlahMahasiswaPerempuan = 27
+Diagram1 = '*'*JumlahMahasiswaLakiLaki
+Diagram2 = '*'*JumlahMahasiswaPerempuan
+print(Diagram1+"57")
+print(Diagram2+"27")
