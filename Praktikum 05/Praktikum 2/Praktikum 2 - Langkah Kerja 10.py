ea139187d27cf3b2baefb992fa635bab77cb92e2
Fahira Zahra <fahirazhra@gmail.com>
2020-11-01 18:56:18 +0700

    Create Praktikum 2 - Langkah Kerja 10.py

---
+++ Praktikum 05/Praktikum 2/Praktikum 2 - Langkah Kerja 10.py
@@ -0,0 +1,13 @@
+# Modifikasi pertama kotak bintang ajaib
+kolom = 0
+baris = 6
+
+i = 0
+while(i < baris):
+    j = 0
+    while(j < kolom):
+        print('* ', end='')
+        j += 1
+    print('')
+    i += 1
+    kolom += 1
