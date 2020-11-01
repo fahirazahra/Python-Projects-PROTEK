ec40e0e58a68c37444e50de67e73b314222ec7c3
Fahira Zahra <fahirazhra@gmail.com>
2020-11-01 18:55:41 +0700

    Create Praktikum 1 - Latihan 1.py

---
+++ Praktikum 05/Praktikum 1/Praktikum 1 - Latihan 1.py
@@ -0,0 +1,19 @@
+# Script Status Kelulusan Mahasiswa
+
+nilaiBI = int(input('Masukkan nilai Bahasa Indonesia : '))
+nilaiIPA = int(input('Masukkan nilai IPA              : '))
+nilaiMTK = int(input('Masukkan nilai Matematika       : '))
+
+
+
+if((nilaiBI >= 0) and (nilaiBI <= 100)) and ((nilaiIPA >= 0) and (nilaiIPA <= 100)) and ((nilaiMTK >= 0) and (nilaiMTK <= 100)):
+        if(nilaiBI >= 60) and (nilaiIPA >= 60) and (nilaiMTK >= 70):
+                SK = 'LULUS'
+        else:
+                SK = 'TIDAK LULUS'
+
+        print('Status Kelulusan                : ' + SK)
+
+
+
+
