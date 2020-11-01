f67c14670e1ac5a5545a21ce9336ff6195a1a00b
Fahira Zahra <fahirazhra@gmail.com>
2020-11-01 18:57:07 +0700

    Create Praktikum 2 - Latihan 4.py

---
+++ Praktikum 05/Praktikum 2/Praktikum 2 - Latihan 4.py
@@ -0,0 +1,12 @@
+# Kotak Bintang Ajaib
+kolom = 6
+baris = 5
+
+i = 0
+while(i < baris):
+    j = i + 1
+    while(j < kolom):
+        print('* ', end='')
+        j += 1
+    print('')
+    i += 1
