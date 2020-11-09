04dba99753ade8c18e11bdd1f5b695217050aa08
Fahira Zahra <fahirazhra@gmail.com>
2020-11-09 21:27:15 +0700

    Create Praktikum 2 - Langkah Kerja 7.py

---
+++ Praktikum 06/Praktikum 2/Praktikum 2 - Langkah Kerja 7.py
@@ -0,0 +1,21 @@
+def luasSegitiga2(a, t):
+    luas = a*t/2
+    print('Luas segitiga dg alas ', alas,
+          ' dan tinggi ', tinggi,
+          ' adalah ', luas)
+
+alas = 10
+tinggi = 20
+luasSegitiga2(alas, tinggi)
+
+# Modifikasi soal Praktikum 2 - Langkah Kerja 3
+
+def luasSegitiga3(a,t):
+    luas2 = a*t/2
+    print('Luas segitiga dg alas', alas,
+          'dan tinggi', tinggi,
+          'adalah', luas2)
+
+alas = 15
+tinggi = 45
+luasSegitiga3(alas, tinggi)
