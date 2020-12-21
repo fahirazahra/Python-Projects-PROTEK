540e7595a7b9b268a7a60776b08363da435e5c51
Fahira Zahra <fahirazahra@student.uns.ac.id>
2020-12-22 02:54:02 +0700

    Update Latihan no. 1.py

--- Praktikum 09/Latihan no. 1.py
+++ Praktikum 09/Latihan no. 1.py
@@ -1,6 +1,8 @@
 # Script kode program mengubah huruf 't' pada kata 'Matematika' dengan perintah replace()
 
 def ubahHuruf (teks, a, b):
+
     return teks.replace(a, b)
+    
 huruf = 'Matematika'
 print(ubahHuruf(huruf, 't', 's'))
